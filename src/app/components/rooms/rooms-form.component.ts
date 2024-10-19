3f47d992f853b4ec6eb17168b80ad80bb640d17e
JulioKrack <julio.ruiz.dev@gmail.com>
2024-10-19 15:06:16 -0500

    con form

--- src/app/components/rooms/rooms-form.component.ts
+++ src/app/components/rooms/rooms-form.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rooms-form',
   templateUrl: './rooms-form.component.html',
   styleUrl: './rooms-form.component.css'
 })
-export class RoomsFormComponent {
+export class RoomsFormComponent implements OnInit {
+
+  titulo = 'Formulario de habitaciones';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
 
 }
